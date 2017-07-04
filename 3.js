bad852e3e384c5c9f39e0d9f4f79d6da41f4d0d5
shandiangou <shandiangou@shandiangoudeMacBook-Pro.local>
2017-07-04 18:32:19 +0800

    first commit

---
+++ 3.js
@@ -0,0 +1,7 @@
+ReactDOM.render(
+            <div>
+                <h1>这是我的第二个例子</h1>
+                <div>你觉得怎么样</div>
+            </div>,
+            document.getElementById('haha')
+        );
